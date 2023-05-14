9f4de586e4621350a0416f131604e26717390249
Yachtzee <edwinalban@gmail.com>
2023-05-14 12:27:57 -0700

    added join method to remove array output and comma separators from password

--- script.js
+++ script.js
@@ -13,107 +13,107 @@
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var specialCharacters = [
-  "@",
-  "%",
-  "+",
-  "\\",
-  "/",
-  "'",
-  "!",
-  "#",
-  "$",
-  "^",
-  "?",
-  ":",
-  ",",
-  ")",
-  "(",
-  "}",
-  "{",
-  "]",
-  "[",
-  "~",
-  "-",
-  "_",
-  ".",
-];
-
-var numericCharacters = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-];
-
-var lowerCasedCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-var upperCasedCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
-
-var possibleCharacters = [];
-
 function generatePassword() {
   
+  var specialCharacters = [
+    "@",
+    "%",
+    "+",
+    "\\",
+    "/",
+    "'",
+    "!",
+    "#",
+    "$",
+    "^",
+    "?",
+    ":",
+    ",",
+    ")",
+    "(",
+    "}",
+    "{",
+    "]",
+    "[",
+    "~",
+    "-",
+    "_",
+    ".",
+  ];
+  
+  var numericCharacters = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  
+  var lowerCasedCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  
+  var upperCasedCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  
+  var possibleCharacters = [];
+
     var charLength = prompt("Please choose the number of characters for your password. It must be at least 8 and no more than 128 characters");
     var confirmLength = confirm("Your password will be " + charLength + " characters.");
     console.log(charLength);
@@ -123,40 +123,40 @@ function generatePassword() {
     console.log(lowercased);
 
       if (lowercased === lowercasedChoice[0]) {
-        var lowercaseConfirm = confirm("Lowercased letters will be used!")
+        var lowercaseConfirm = confirm("Lowercased letters will be used!");
         possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
     } else {
-        var lowercaseDeny = confirm("Lowercased letters will not be used.")
+        var lowercaseDeny = confirm("Lowercased letters will not be used.");
     };
 
     var uppercased = prompt("Do you want to include uppercased letters? Please type 'Yes', or 'No'.").toLowerCase();
     var uppercasedChoice = ["yes", "no"];
     console.log(uppercased);
       if (uppercased === uppercasedChoice[0]) {
-        var uppercaseConfirm = confirm("Uppercased letters will be used!")
+        var uppercaseConfirm = confirm("Uppercased letters will be used!");
         possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
     } else {
-      var uppercaseDeny = confirm("Uppercased letters will not be used.")
+      var uppercaseDeny = confirm("Uppercased letters will not be used.");
     };
 
     var numeric = prompt("Do you want to include numbers? Please type 'Yes', or 'No'.").toLowerCase();
     var numericChoice = ["yes", "no"];
     console.log(numeric);
       if (numeric === numericChoice[0]) {
-        var numericConfirm = confirm("Numbers will be used!")
+        var numericConfirm = confirm("Numbers will be used!");
         possibleCharacters = possibleCharacters.concat(numericCharacters);
     } else {
-      var numericDeny = confirm("Numbers will not be used.")
+      var numericDeny = confirm("Numbers will not be used.");
     };
 
     var special = prompt("Do you want to include special characters? Please type 'Yes', or 'No'.").toLowerCase();
     var specialChoice = ["yes", "no"];
     console.log(special);
       if (special === specialChoice[0]) {
-        var specialConfirm = confirm("Special characters will be used!")
+        var specialConfirm = confirm("Special characters will be used!");
         possibleCharacters = possibleCharacters.concat(specialCharacters);
     } else {
-      var specialDeny = confirm("Special characters will not be used.")
+      var specialDeny = confirm("Special characters will not be used.");
     };
 
    console.log(possibleCharacters);
@@ -170,7 +170,10 @@ function generatePassword() {
       password.push(randElement);
     }
     console.log(password);
-}
+    password.join('');
+    console.log(password.join(''));
+    password = password.join('');
+};
 
 
 
